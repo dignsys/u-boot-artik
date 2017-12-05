1dc1be7e4ac6c6835cf8a5bda36377ca7f6f6fbf
Jaehoon Chung <jh80.chung@samsung.com>
2017-12-05 19:17:24 +0900

    scripts: tizen: artik530s: add the prebuilt image for os_3.0.0
    Add the prebuilt image to migrate to OS_3.0.0 on ARTIK530S target.
    
    Change-Id: I1071d7f8be6b34fc5438970bc13497c966ee0a5c
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>

---
+++ scripts/tizen/artik530s/os_3.0.0/flash_boot_for_tizen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Fusing bootloader binaries..."
+sudo fastboot flash partmap ./partmap_artik530s_tizen_emmc.txt
+sudo fastboot flash 2ndboot ./bl1-emmcboot.img
+sudo fastboot flash loader ./loader-emmc.img
+sudo fastboot flash blmon ./bl_mon.img
+sudo fastboot flash bootloader ./bootloader.img
+sudo fastboot flash env ./tizen_params.bin
+
+sudo fastboot reboot
+
+echo "Fusing done"
+echo "You have to resize the rootfs after first booting"

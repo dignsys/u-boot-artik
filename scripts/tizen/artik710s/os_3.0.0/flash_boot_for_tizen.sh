767d14586a82d528b128c7d63208d7f54e01a447
Jaehoon Chung <jh80.chung@samsung.com>
2017-12-06 13:26:01 +0900

    script: tizen: artik710s: add the prebuilt image for tizen
    ARTIK710S is introduced since OS_3.0.0.
    It needs to migrate to tizen specific image from DS version.
    
    This patch is for providing the tizen specific images.
    
    Read the README.A710S_os_3.0.0.
    
    Change-Id: I412e0e695f19dc80b3bdc111d7455d664f9ba368
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>

---
+++ scripts/tizen/artik710s/os_3.0.0/flash_boot_for_tizen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Fusing ARTIK710 OS_3.0.0 bootloader binaries..."
+sudo fastboot flash partmap ./partmap_artik710s_tizen_emmc.txt
+sudo fastboot flash 2ndboot ./bl1-emmcboot.img
+sudo fastboot flash fip-loader ./fip-loader-emmc.img
+sudo fastboot flash fip-nonsecure ./fip-nonsecure.img
+sudo fastboot flash env ./tizen_params.bin
+sudo fastboot flash boot ./boot.img
+
+sudo fastboot reboot
+
+echo "Fusing done"
+echo "You have to resize the rootfs after first booting"
